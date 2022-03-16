39b9884cf46105003e444d6aefe16f073fdc5a51
akshay.ams96 <akshaysuryawanshi9696@gmail.com>
2022-03-16 16:04:30 +0530

    Revert "added new messge inside the good return"
    This reverts commit 531f3113a164f6789693ca8e3e7c29803a655ee1.

--- howru.py
+++ howru.py
@@ -4,10 +4,8 @@ user_input = input("how are you==").lower()
 print(user_input)
 if user_input == "good":
      print("thanks!,nice to hear that!!")
-     print("so u dont need help!!")
 elif user_input=="bad":
     print("hey!!are you ok!!")
-    print("you need help!!")
 
 else:
      print("invalid input please enter your feedback in good/bad answer")
