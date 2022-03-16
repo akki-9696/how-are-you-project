59cadab84ea5e6f7ec6a5b4d5e6a429a4eb56c11
akshay.ams96 <akshaysuryawanshi9696@gmail.com>
2022-03-16 17:55:35 +0530

    coding inside the new user

--- howru.py
+++ howru.py
@@ -3,10 +3,14 @@ print("please enter your feedback in good/bad answer")
 user_input = input("how are you==").lower()
 print(user_input)
 if user_input == "good":
+     print("This is a good replay code")
      print("thanks!,nice to hear that!!")
+     print("can u help me if you dont mind!")
 elif user_input=="bad":
+    print("this is the bad repay code")
     print("hey!!are you ok!!")
     print("do u want some help")
+    print("i can help you to come outoff your problem")
 
 else:
     print("please provide your outputin yes/no ")
