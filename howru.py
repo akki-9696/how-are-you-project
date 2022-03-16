2965f5a55b0cc85658ad50b7b1b7ef04d1452206
akshay.ams96 <akshaysuryawanshi9696@gmail.com>
2022-03-16 15:22:02 +0530

    first commit of howareyou project

---
+++ howru.py
@@ -0,0 +1,11 @@
+#this is our basic project for it hub
+print("please enter your feedback in good/bad answer")
+user_input = input("how are you==").lower()
+print(user_input)
+if user_input in ("good","bad"):
+     print("thanks for your feedback")
+
+else:
+     print("invalid input please enter your feedback in good/bad answer")
+   
+   
